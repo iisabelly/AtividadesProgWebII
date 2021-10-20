163f791dae2b90c94548c21e87d343552a66a852
iisabelly <91489199+iisabelly@users.noreply.github.com>
2021-10-19 21:06:15 -0300

    Update Atividade01js.js

--- Atividade01js.js
+++ Atividade01js.js
@@ -1,18 +1,27 @@
-    const topicos = document.querySelectorAll('h2');
-    const topicoslista = new Array();
-    topicosindividuais = new Object();
-    topicosindividuais = document.getElementById('topico01').textContent;
-    topicoslista.push(topicosindividuais);
-    topicosindividuais = document.getElementById('topico02').textContent;
-    topicoslista.push(topicosindividuais);
-    topicosindividuais = document.getElementById('topico03').textContent;
-    topicoslista.push(topicosindividuais);
+    const topicosh2 = document.querySelectorAll('h2');
+    const topicos = new Array();
 
-    alista = "<ol start='1'><li><a href='#topico01'>"+topicoslista[0]+"</a></li><li><a href='#topico02'>"+topicoslista[1]+"</a></li><li><a href='#topico03'>"+topicoslista[2]+"</a></li></ol>"
+    topicosh2.forEach(elementos =>{
+        topicos.push(elementos.textContent);
 
-    document.getElementById("lista").innerHTML = alista;
+        const topo = document.createElement('a');
+        topo.setAttribute('name', elementos.textContent);
+        elementos.append(topo);
 
-    olinklista = "<br><a href='#lista'>voltar</a>"
-    document.getElementById("topico01").innerHTML += olinklista;
-    document.getElementById("topico02").innerHTML += olinklista;
-    document.getElementById("topico03").innerHTML += olinklista;
+        const voltar = document.createElement('a');
+        voltar.setAttribute('href', '#');
+        voltar.textContent = "Voltar ao topo";
+        
+        elementos.parentElement.insertBefore(voltar, elementos.nextElementSibling);
+    });
+
+    const lista = document.querySelector("ol");
+    topicos.forEach(topico =>{
+        const topicolista = document.createElement('li');
+        lista.append(topicolista);
+
+        const topicoslink = document.createElement('a');
+        topicoslink.setAttribute('href', `#${topico}`);
+        topicoslink.textContent = topico;
+        topicolista.append(topicoslink);
+    });
